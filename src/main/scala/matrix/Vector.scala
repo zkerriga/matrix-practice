b7606b6eab22510bac9b1451b54f223fcd076c8e
Zandra Kerrigan <zkerriga@f0r4s9.codam.nl>
2023-05-13 20:12:56 +0200

    Add some convinient constructors

--- src/main/scala/matrix/Vector.scala
+++ src/main/scala/matrix/Vector.scala
@@ -2,14 +2,28 @@ package matrix
 
 import scala.Tuple.{Size, Union}
 
-
 trait Vector[Size <: Int, A]:
   def size: Size
 
 object Vector:
-  class Impl[Size<: Int, A](size0: Size) extends Vector[Size, A]:
+  private class Impl[Size <: Int, A](size0: Size, array: Array[A]) extends Vector[Size, A]:
     def size: Size = size0
 
+    override def toString: String = array.mkString("[", ", ", "]")
+  end Impl
+
+  def apply(tuple: NonEmptyTuple): Vector[Tuple.Size[tuple.type], Tuple.Union[tuple.type]] =
+    Impl(tuple.size, tuple.toArray.asInstanceOf[Array[Tuple.Union[tuple.type]]])
+
+  def make[A](tuple: NonEmptyTuple)(using
+    Tuple.Union[tuple.type] =:= A
+  ): Vector[Tuple.Size[tuple.type], A] =
+    apply(tuple).asInstanceOf[Vector[Tuple.Size[tuple.type], A]]
+
+  def makeSized[Size <: Int, A](
+    tuple: NonEmptyTuple
+  )(using Tuple.Union[tuple.type] =:= A, Tuple.Size[tuple.type] =:= Size): Vector[Size, A] =
+    apply(tuple).asInstanceOf[Vector[Size, A]]
 
 @main def test = {
   import scala.compiletime.ops.int.*
@@ -18,7 +32,9 @@ object Vector:
 
   def test[Size <: Int & Singleton](size: Size)(using Size > 0 =:= true): Size = size
 
-  def test2[Size <: Int & Singleton, Tup <: Tuple](t: Tup)(using Size > 0 =:= true, Tuple.Size[Tup] =:= Size): Unit = ()
+  def test2[Size <: Int & Singleton, Tup <: Tuple](
+    t: Tup
+  )(using Size > 0 =:= true, Tuple.Size[Tup] =:= Size): Unit = ()
 
   class Partial[Size <: Int & Singleton]() {
     def apply[Tup <: Tuple](t: Tup)(using Tuple.Size[Tup] =:= Size): Unit = ???
@@ -34,11 +50,12 @@ object Vector:
 
   type AllElementsOfType[X <: Tuple, A] <: Boolean = X match {
     case EmptyTuple => true
-    case x1 *: xs1 => (x1 == A) && AllElementsOfType[xs1, A]
+    case x1 *: xs1  => (x1 == A) && AllElementsOfType[xs1, A]
   }
 
 //  def test5[Tup <: Tuple](t: Tup)(using AllElementsOfType[Tup, Tuple.Head[Tup]] =:= true): (Tuple.Size[Tup], List[Tuple.Union[Tup]]) = (t.size, t.toList)
-  def test6[Tup <: Tuple](t: Tup): (Tuple.Size[Tup], List[Tuple.Union[Tup]]) = (t.size, t.toList.asInstanceOf[List[Tuple.Union[Tup]]])
+  def test6[Tup <: Tuple](t: Tup): (Tuple.Size[Tup], List[Tuple.Union[Tup]]) =
+    (t.size, t.toList.asInstanceOf[List[Tuple.Union[Tup]]])
 
   def test7(t: Tuple): (Tuple.Size[t.type], List[Tuple.Union[t.type]]) = (t.size, t.toList)
 
@@ -47,7 +64,14 @@ object Vector:
 //  val result = test4(('a', 'b'))
 //  val result = test5(('a', 'b', 'c'))
 //  val result: (3, List[Char]) = test6(('a', 'b', 'c'))
-  val result: (3, List[Char]) = test7(('a', 'b', 'c'))
+//  val result: (3, List[Char]) = test7(('a', 'b', 'c'))
 
-  println(s"result = ${result}")
+//  println(s"result = ${result}")
+
+  val vector3: Vector[3, Double] = Vector((1.2, 4.0, 1.0))
+
+  val vector2: Vector[3, Double] = Vector.make[Double]((1.0, 3.5, 5.4))
+  val vector1: Vector[3, Double] = Vector.makeSized[3, Double]((1.0, 3.5, 5.4))
+
+  println(vector3)
 }
