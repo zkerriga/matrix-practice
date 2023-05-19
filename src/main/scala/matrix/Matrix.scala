ae1df1c06f341966450e3afe56ec211a7a59289d
Daniil Eletskii <deletskii@evolution.com>
2023-05-19 20:27:39 +0200

    remove everything to start again

--- src/main/scala/matrix/Matrix.scala
+++ src/main/scala/matrix/Matrix.scala
@@ -1,45 +1,5 @@
 package matrix
 
-import utils.ProductA
+trait Matrix
 
-import scala.compiletime.ops.any.==
-
-trait Matrix[Weight <: Int & Singleton, Height <: Int & Singleton, +A]:
-  inline final def shape: (Weight, Height) = valueOf[Weight] -> valueOf[Height]
-  inline final def isSquare: Boolean       = valueOf[Weight == Height]
-
-  def *[A1 >: A, B](scalar: B)(using ProductA[A1, B]): Matrix[Weight, Height, A1]
-
-object Matrix:
-  private class Impl[Weight <: Int & Singleton, Height <: Int & Singleton, +A](
-    table: Vector[Height, Vector[Weight, A]]
-  ) extends Matrix[Weight, Height, A]:
-    def *[A1 >: A, B](scalar: B)(using ProductA[A1, B]): Matrix[Weight, Height, A1] = {
-      given ProductA[Vector[Weight, A1], B] = _ * _
-      Impl[Weight, Height, A1](table * scalar)
-    }
-
-    override def toString: String = table.toString
-  end Impl
-
-  def apply[Weight <: Int & Singleton, Height <: Int & Singleton, A](
-    table: Vector[Height, Vector[Weight, A]]
-  ): Matrix[Weight, Height, A] = Impl(table)
-
-@main def test = {
-  val matrix: Matrix[3, 4, Int] = Matrix {
-    Vector.make[4](
-      (
-        Vector.make[3]((1, 4, 5)),
-        Vector.make[3]((4, 1, 4)),
-        Vector.make[3]((0, 1, 4)),
-        Vector.make[3]((0, 14, 4)),
-      )
-    )
-  }
-
-  println(s"result = $matrix")
-  println(s"shape = ${matrix.shape}")
-  println(s"square = ${matrix.isSquare}")
-  println(s"* = ${matrix * 100}")
-}
+object Matrix
--- src/main/scala/matrix/TupleUtils.scala
+++
@@ -1,8 +0,0 @@
-package matrix
-
-object TupleUtils {
-  type Size[X <: Tuple] <: Int & Singleton = X match {
-    case EmptyTuple => 0
-    case x *: xs    => SingletonIntPlus[Size[xs], 1]#Result
-  }
-}
